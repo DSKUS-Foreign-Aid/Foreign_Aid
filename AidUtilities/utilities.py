af0cc53619e588e5c363898aaae17c27a159f908
1sandovalgerardo <1sandoval.gerardo@gmail.com>
2022-04-17 15:42:09 -0500

    Added reference files for series names

--- AidUtilities/utilities.py
+++ AidUtilities/utilities.py
@@ -24,3 +24,47 @@ def clean_col_names(df, old_char=[' '], new_char=['_']):
                 new_col_names.append(col.replace(old, new))
     df.columns = new_col_names
     return df
+
+
+def create_series_names(series_names, return_tuple=False):
+    '''Designed to take in World Bank dataframe with multiple series(studies).
+    Then ask for user input on what the new series name should be.
+    If "quit" is entered as a value, the function exits and returns input values to that point.
+    Args:
+        series_names(pandas.series): expects a series of world bank series
+        return_tuple(bool): 
+            if True--returns a list of tuples. 
+                the tuples contain (old_series_name, new_series_name)
+            if False-- returns a list of strings that correspond to 
+                the new name for each series
+    Returns:
+        List of either string elements of tuples.  The return object is based on return_tuple value.
+    '''
+    new_series_names = []
+    for series in series_names:
+        print(series)
+        new_name = input('new name: ')
+        if new_name == 'quit':
+            return print(new_series_names)
+        # return a list of tuples. 
+        if return_tuple:
+            new_series_names.append((old_series, new_name))
+        else:
+            new_series_names.append(new_name)
+    return new_series_names
+
+
+def print_cols(aList, cols=3):
+    '''Enter a list and will print the items in
+    an organized column/s.'''
+    count = 0
+    aList.sort()
+    for i in aList:
+        print(f'{str(i): <30}\t', end='')
+        count += 1
+        if count > (cols-1):
+            print()
+            count = 0
+    return None
+
+    
